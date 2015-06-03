10abdd5a23f65763ce5ed4b74941f92248051b1b
Eduardo <edubaik@gmail.com>
2015-06-03 13:04:29 -0700

    first commit

---
+++ application/controllers/Leads.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Leads extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('leads');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
